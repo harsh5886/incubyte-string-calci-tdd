fe9be203780e9933177a497450921a65101effea
harsh5886 <101454029+harsh5886@users.noreply.github.com>
2024-08-13 13:34:11 +0530

    Intial unit test commit

---
+++ src/test/java/net/calculator/tdd/TestCalculator.java
@@ -0,0 +1,13 @@
+package net.calculator.tdd;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestCalculator {
+
+    @Test
+    public void testCalculator() {
+        assertEquals(0, 0);
+    }
+}
